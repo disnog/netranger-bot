525e9050ebcdf9140a36643f3d7e06af8643e5c6
Jason R. Rokeach <jason@rokeach.net>
2019-08-22 21:57:39 -0400

    Initial commit

---
+++ network-ranger/__main__.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
+#  network-ranger - __main__.py
+#  Copyright (C) 2019  Jason R. Rokeach
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Connect as a Discord bot to maintain the Networking Discord Server.
+"""
+
+# TODO: Fix these lazy hard set global variables
+command_prefix='$'
+bot_description='The in-development bot which will maintain the Networking Discord server'
+
+import discord
+import argparse
+from discord.ext import commands
+
+parser = argparse.ArgumentParser(fromfile_prefix_chars="@", formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-t','--token', help='Discord API Token', required=True)
+args = vars(parser.parse_args())
+
+bot = commands.Bot(command_prefix=command_prefix,description=bot_description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def info(ctx):
+    embed=discord.Embed(title='Network Ranger',description=bot_description)
+    embed.add_field(name='Author',value='Jason')
+    await ctx.send(embed=embed)
+
+bot.run(args['token'])
