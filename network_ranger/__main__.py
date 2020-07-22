ee07f9aa572aad33888ac672cb5c02e9a3c84dd3
Jason R. Rokeach <jason@rokeach.net>
2020-07-22 00:13:05 -0400

    Less verbose welcome for accepted members.

--- network_ranger/__main__.py
+++ network_ranger/__main__.py
@@ -435,9 +435,7 @@ async def accept(ctx, answer: str = None):
         db.add_member_numbers()
         await memberchannel.send(
             "{mention}, welcome to {server}! You are member #{membernumber}, and we're glad to have you. Feel free to "
-            "take a moment to introduce yourself! If you want to rep your company or school based on your email domain,"
-            " get a key by DMing me the command ```{command_prefix}sendkey <email>``` then set an org role using: "
-            "```{command_prefix}role org set <key>``` in any channel".format(
+            "take a moment to introduce yourself!".format(
                 mention=ctx.author.mention,
                 server=memberchannel.guild.name,
                 membernumber=db.get_member_number(ctx.author.id),
