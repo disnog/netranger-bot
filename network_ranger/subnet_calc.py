da93a184e2aac21b428895d5ed9a3803d7ff2cd4
Ludwig Nilsson <61899183+1Ludwig@users.noreply.github.com>
2022-08-01 01:15:11 +0200

    Update subnet_calc.py
    a duplicate function found.
    removing the old subnet_collision_checker_function()

--- network_ranger/subnet_calc.py
+++ network_ranger/subnet_calc.py
@@ -127,33 +127,6 @@ def subnet_collision_checker_function():
         return error_message
 
 
-# Checks args list and if passed runs the calculation
-# usecase args: 10.10.1.5/23 10.10.2.5/21
-def subnet_collision_checker_function():
-    if len(argumentList) > 1:
-        input_validation = re.match(
-            RP_FULLCIDRv4, argumentList[0] + " " + argumentList[1]
-        )
-
-        if input_validation:
-            subnet_1 = ipcalc.Network(argumentList[0])
-            subnet_2 = ipcalc.Network(argumentList[1])
-            check_subnets = subnet_1.check_collision(subnet_2)
-            if check_subnets:
-                return "IP address {} and {} is in the same subnet.".format(
-                    subnet_1, subnet_2
-                )
-            elif check_subnets == False:
-                return "IP address {} and {} is not the same subnet.".format(
-                    subnet_1, subnet_2
-                )
-        else:
-            error_message = (
-                "Invalid input" "\nuse syntax:" "\n-ipcc 10.10.10.5/29 10.10.10.16/28"
-            )
-            return error_message
-
-
 # Regular Expressions
 RP_IPV4FULLMASK = r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)( )((((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3})))$"
 RP_FULLCIDRv4 = r"^((?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)(\/)(0?[1-9]|[12]\d|3[012]) ?)+$"
