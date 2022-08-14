eece25c7631b96597e0558328029f87a234daec4
Ludwig Nilsson <61899183+1Ludwig@users.noreply.github.com>
2022-08-14 03:03:20 +0200

    Update subnet_calc.py (#86)
    * Update subnet_calc.py
    
    a duplicate function found.
    removing the old subnet_collision_checker_function()
    
    * Update subnet_calc.py

--- network_ranger/subnet_calc.py
+++ network_ranger/subnet_calc.py
@@ -106,15 +106,16 @@ def subnet_collision_checker_function():
 
         # if either of the inputs are an IPv6 address, then compress the result.
         if subnet_1.version() == 6:
-            subnet_1 = subnet_1.to_compressed()
+            subnet_1 = f"{subnet_1.to_compressed()}/{subnet_1.subnet()}"
+
         if subnet_2.version() == 6:
-            subnet_2 = subnet_2.to_compressed()
+            subnet_2 = f"{subnet_2.to_compressed()}/{subnet_2.subnet()}"
 
         # Boolean checks and return a message depending on the statement.
         if check_subnets:
             return f"IP address {subnet_1} and {subnet_2} is in the same subnet."
         elif check_subnets == False:
-            return f"IP address {subnet_1} and {subnet_2} is not the same subnet."
+            return f"IP address {subnet_1} and {subnet_2} is not in the same subnet."
 
     except Exception as e:
         error_message = (
@@ -125,33 +126,6 @@ def subnet_collision_checker_function():
             "\n-ipcalc collision 2001:db8:2000::1/64 2001:db8:2001::10/64"
         )
         return error_message
-
-
-# Checks args list and if passed runs the calculation
-# usecase args: 10.10.1.5/23 10.10.2.5/21
-def subnet_collision_checker_function():
-    if len(argumentList) > 1:
-        input_validation = re.match(
-            RP_FULLCIDRv4, argumentList[0] + " " + argumentList[1]
-        )
-
-        if input_validation:
-            subnet_1 = ipcalc.Network(argumentList[0])
-            subnet_2 = ipcalc.Network(argumentList[1])
-            check_subnets = subnet_1.check_collision(subnet_2)
-            if check_subnets:
-                return "IP address {} and {} is in the same subnet.".format(
-                    subnet_1, subnet_2
-                )
-            elif check_subnets == False:
-                return "IP address {} and {} is not the same subnet.".format(
-                    subnet_1, subnet_2
-                )
-        else:
-            error_message = (
-                "Invalid input" "\nuse syntax:" "\n-ipcc 10.10.10.5/29 10.10.10.16/28"
-            )
-            return error_message
 
 
 # Regular Expressions
